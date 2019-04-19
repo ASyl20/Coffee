554f1fd839861700c4e7721777bb16092133d92b
Soulkaiser <ksoulkaiser@gmail.com>
2019-04-19 11:40:27 +0200

    Navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./layout.css"
 import "./bootstrap.min.css"
+import Navbar from "./Globals/Navbar"
 
 const Layout = ({ children }) => (
  <>
+ <Navbar/>
  {children}
  </>
 )
